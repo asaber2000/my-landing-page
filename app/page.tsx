5e5cae40d9d050279edecf6200103dee5bbfced2
Ahmed Saber <ahmedssaabber22@gmail.com>
2026-09-10 16:22:43 +0400

    Integrate official Google Tag Manager via next third-parties

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Hero from "@/components/Hero";
 import FeaturedSolutions from "@/components/FeaturedSolutions";
-import ScrollTrigger from "@/components/ScrollTrigger";
 import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
 
@@ -10,7 +9,6 @@ export default function Home() {
     <main className="min-h-screen bg-[#070B14] text-white">
       <Hero />
       <FeaturedSolutions />
-      <ScrollTrigger />
       <ContactSection />
       <Footer />
     </main>
