8b9a8055abbaad6d91a1604eae32610a607e5194
Ahmed Saber <ahmedssaabber22@gmail.com>
2026-09-26 13:47:14 +0400

    update rebots.txt for OAI-SearchBot, OAI-AdsBot

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,18 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/'],
+      },
+      {
+        userAgent: ['OAI-SearchBot', 'OAI-AdsBot'],
+        allow: '/',
+        disallow: ['/api/'],
+      },
+    ],
     sitemap: 'https://tents.baitalnokhada.com/sitemap.xml',
   };
 }
